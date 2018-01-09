3cbe4f95a91fd17f5959b1166233fc00dcf107c7
dameng <pingf0@gmail.com>
2018-01-09 22:45:11 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""
+termcc
+-------------
+
+This is the description for that library
+"""
+from setuptools import setup
+
+
+setup(
+    name='termcc',
+    version='2018.01.03',
+    url='https://github.com/pingf/termcc.git',
+    license='BSD',
+    author='Jesse MENG',
+    author_email='pingf0@gmail.com',
+    description='Very short description',
+    long_description=__doc__,
+    py_modules=['termcc'],
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    packages=['termcc'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        # 'Flask'
+    ],
+    scripts=['bin/termcc_show.py'],
+    entry_points={'console_scripts': [
+        'termcc = termcc_show:show',
+    ]},
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ test/simple.py
@@ -0,0 +1,8 @@
+from termcc.cc import cc
+
+from termcc.core import ccc
+
+if __name__ == '__main__':
+    m = ccc('yin yang', fore='red')
+    print(m)
+    print(cc(':red: :yin_yang: :reverse_green: hello world'))
